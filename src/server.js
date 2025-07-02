e7cd156c291f710bcca40164a2bb0a008eba053f
gurungnavin <gurungnavin@icloud.com>
2025-07-02 13:22:16 +0900

    cron install for api request for every 14 minutes if we are in production mode

--- src/server.js
+++ src/server.js
@@ -3,12 +3,22 @@ import "dotenv/config";
 import { connetDB } from "./config/db.js";
 import rateLimiterMiddleware from "./middleware/rateLimiter.js";
 import router from "./routes/transaction.router.js";
+import job from "./config/cron.js"
+
 const app = express();
 
+if (process.env.NODE_ENV === "production") job.start(); // Start cron job only in production environment
+
+//middleware
 app.use(rateLimiterMiddleware) // Apply rate limiting middleware to all routes
 app.use(express.json()); // Middleware to parse JSON bodies
+
+// Port configuration
 const PORT = process.env.PORT || 5001;
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Server is healthy" });
+})
 
 app.use("/api/transactions", router)
 
